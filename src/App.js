ce0e9deb830cbb8cf5e212512bb18fe51ed916e2
Oleg <oleg.vintonuk@gmail.com>
2018-10-10 16:13:14 +0300

    Changed App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import TrainingCard from "./components/cards/TrainingCard";
-import OrganisatinsCard from "./components/cards/OrganisatinsCard";
-import MemberCard from "./components/cards/MemberCard";
 import PageContainer from "./components/pages/PageContainer";
 
 class App extends Component {
@@ -12,10 +9,6 @@ class App extends Component {
       <div>
         <h1>Hello Project</h1>
         <PageContainer />
-
-        <TrainingCard />
-        <OrganisatinsCard />
-        <MemberCard />
       </div>
     );
   }
