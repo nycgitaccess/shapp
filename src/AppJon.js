8d7e04aaec3d797029b86f4639f86021a6d8040e
miguelpel <pelleteratdeborde@yahoo.fr>
2018-10-11 16:57:34 +0300

    Cleared the App.js file

--- src/AppJon.js
+++ src/AppJon.js
@@ -5,12 +5,10 @@ import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
 import * as routes from './constants/routes';
 
 import NavBar from './components/menu/NavBar';
-import Trainigns from './components/pages/placeholders/Trainings';
-import Organisations from './components/pages/placeholders/Organisations';
-import Members from './components/pages/placeholders/Members';
-import Manage from './components/pages/placeholders/Manage';
-
-import Trainings from './components/pages/placeholders/Trainings';
+import Trainings from './components/pages/Trainings';
+import Organisations from './components/pages/Organisations';
+import Members from './components/pages/Members';
+import Manage from './components/pages/Manage';
 
 
 class App extends Component {
@@ -19,19 +17,15 @@ class App extends Component {
       <Router>
         <div className="AppContent">
           <div className="menu">
-            <NavBar />
-            <Trainings />
-            <Organisations />
-            <Members />
-            <Manage />
-              
-            </div>
-            <div className="contnetFetch">
-              <Route exact path={routes.TRAININGS} component={Trainigns} /> 
-                <Route exact path={routes.ORGANISATIONS} component={Organisations} />
-                <Route exact path={routes.MEMBERS} component={Members} /> 
-                <Route exact path={routes.MANAGE} component={Manage} />
-            </div>         
+            <NavBar /> 
+          </div>
+
+          <div className="page">
+              <Route exact path={routes.TRAININGS} component={Trainings} /> 
+              <Route exact path={routes.ORGANISATIONS} component={Organisations} />
+              <Route exact path={routes.MEMBERS} component={Members} /> 
+              <Route exact path={routes.MANAGE} component={Manage} />
+          </div>         
         </div>
       </Router>
     );
