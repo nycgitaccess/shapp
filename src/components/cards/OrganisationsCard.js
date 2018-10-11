00cf05a26b264e32ee23e3e442f07ffbafcb7f32
Oleg <oleg.vintonuk@gmail.com>
2018-10-11 13:03:37 +0300

    fixed OrganisationsCard

--- src/components/cards/OrganisationsCard.js
+++ src/components/cards/OrganisationsCard.js
@@ -1,30 +1,30 @@
 import React, { Component } from "react";
-import "./organisatinsCard.css";
-import organisatinsCardData from "../../jsonData/organisatinsCard.json";
+import "./organisationsCard.css";
+import organisationsCardData from "../../jsonData/organisationsCard.json";
 
-class OrganisatinsCard extends Component {
+class OrganisationsCard extends Component {
   state = {
-    dataOrganisations: organisatinsCardData
+    dataOrganisations: organisationsCardData
   };
 
   render() {
     const { dataOrganisations } = this.state;
     const printOrganisationsData = dataOrganisations.map(organisation => (
-      <div key={organisation.id} className="organisatinCard">
-        <div className="organisatinCard__left">
+      <div key={organisation.id} className="organisationCard">
+        <div className="organisationCard__left">
           <img
             src={`${organisation.organisationLogo}`}
             alt={`${organisation.acronym}`}
           />
         </div>
 
-        <div className="organisatinCard__center">
+        <div className="organisationCard__center">
           <p>{organisation.acronym}</p>
           <p>{organisation.telephone}</p>
           <p>{organisation.website}</p>
         </div>
 
-        <div className="organisatinCard__right">
+        <div className="organisationCard__right">
           <p>{organisation.city}</p>
           <p>{organisation.country}</p>
           <a className="btn" href="#">
@@ -38,4 +38,4 @@ class OrganisatinsCard extends Component {
   }
 }
 
-export default OrganisatinsCard;
+export default OrganisationsCard;
